6ccd18d8ec80cfe4043ff41dede52d3b90abab9e
Rizki Nugraha <nugraharizki80@gmail.com>
2025-02-23 20:29:19 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="content-wrapper">
+      <h1 className="text-4xl text-teal-500 italic underline">
+        Hello World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
